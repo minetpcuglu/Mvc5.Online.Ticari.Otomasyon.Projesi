30483a04bd45213a6144fbc5f3f7144b29ad9056
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-07-29 17:58:03 +0300

    29.07

--- Mvc5.Online.Ticari.Otomasyon.Projesi/Controllers/SalesMovementsController.cs
+++ Mvc5.Online.Ticari.Otomasyon.Projesi/Controllers/SalesMovementsController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,58 @@ namespace Mvc5.Online.Ticari.Otomasyon.Projesi.Controllers
     {
         // GET: SalesMovements
         SalesMovementsManager SMM = new SalesMovementsManager(new EfSalesMovementsDal());
+        ProductManager PM = new ProductManager(new EfProductDal());
+        EmployeeManager EM = new EmployeeManager(new EfEmployeeDal());
+        CurrentManager CM = new CurrentManager(new EfCurrentDal());
+
         Context c = new Context();
         public ActionResult Index()
         {
+            var deger = SMM.GetList();
+            return View(deger);
+        }
+
+        [HttpGet]
+        public ActionResult NewSales()
+        {
+            List<SelectListItem> degerurun = (from x in PM.GetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductId.ToString()
+                                           }
+                                           ).ToList();
+
+            List<SelectListItem> degercari = (from x in CM.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.CurrentName + " " + x.CurrentSurname ,
+                                                  Value = x.CurrentId.ToString()
+                                              }
+                                         ).ToList();
+
+            List<SelectListItem> degerpersonel = (from x in EM.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.EmployeeName + " " + x.EmployeeSurname,
+                                                  Value = x.EmployeeId.ToString()
+                                              }
+                                         ).ToList();
+
+            ViewBag.deger = degerurun;
+            ViewBag.deger2 = degercari;
+            ViewBag.deger3 = degerpersonel;
+
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewSales(SalesMovements s)
+        {
+            s.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            SMM.SalesMovementAdd(s);
+            return RedirectToAction("Index");
         }
 
         public ActionResult SalesMovementsByEmployee(int id)
@@ -38,5 +87,53 @@ namespace Mvc5.Online.Ticari.Otomasyon.Projesi.Controllers
 
             return View(deger);
         }
+
+        [HttpGet]
+        public ActionResult SalesMovementsUpdatePage(int id)
+        {
+            List<SelectListItem> degerurun = (from x in PM.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.ProductName,
+                                                  Value = x.ProductId.ToString()
+                                              }
+                                           ).ToList();
+
+            List<SelectListItem> degercari = (from x in CM.GetList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.CurrentName + " " + x.CurrentSurname,
+                                                  Value = x.CurrentId.ToString()
+                                              }
+                                         ).ToList();
+
+            List<SelectListItem> degerpersonel = (from x in EM.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.EmployeeName + " " + x.EmployeeSurname,
+                                                      Value = x.EmployeeId.ToString()
+                                                  }
+                                         ).ToList();
+
+            ViewBag.deger = degerurun;
+            ViewBag.deger2 = degercari;
+            ViewBag.deger3 = degerpersonel;
+
+
+
+            var deger = SMM.GetById(id);
+            return View(deger);
+        }
+
+        [HttpPost]
+        public ActionResult SalesMovementsUpdatePage(SalesMovements p)
+        {
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            SMM.SalesMovementUpdate(p);
+          
+
+            return RedirectToAction("Index");
+        }
     }
+
 }
