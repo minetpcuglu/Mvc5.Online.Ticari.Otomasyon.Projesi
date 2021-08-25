3e10afa7def392fbb8f4f48f799d176c1e98aab1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-08-25 22:42:08 +0300

    25.08

--- Mvc5.Online.Ticari.Otomasyon.Projesi/Controllers/MessageController.cs
+++ Mvc5.Online.Ticari.Otomasyon.Projesi/Controllers/MessageController.cs
@@ -21,9 +21,11 @@ namespace Mvc5.Online.Ticari.Otomasyon.Projesi.Controllers
         public ActionResult InBox()
         {
             var mail = (string)Session["CurrentMail"];
-            var deger = mm.GetList().Where(x => x.ReceiverMail == mail).ToList();
+            var deger = mm.GetList().Where(x => x.ReceiverMail == mail).OrderByDescending(x=>x.MessageId).ToList();
             var gelensayısı = mm.GetList().Count(y => y.ReceiverMail == mail).ToString();
+            var gonderilensayısı = mm.GetList().Count(y => y.SenderMail == mail).ToString();
             ViewBag.d1 = gelensayısı;
+            ViewBag.d2 = gonderilensayısı;
             return View(deger);
         }
 
@@ -35,7 +37,35 @@ namespace Mvc5.Online.Ticari.Otomasyon.Projesi.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message m)
         {
-            return View();
+            var mail = (string)Session["CurrentMail"];
+            m.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            m.SenderMail = mail;
+            mm.MessageAdd(m);
+            return RedirectToAction("SendBox");
         }
+
+        public ActionResult SendBox()
+        {
+            var mail = (string)Session["CurrentMail"];
+            var deger = mm.GetList().Where(x => x.SenderMail == mail).OrderByDescending(x => x.MessageId).ToList();
+            var gelensayısı = mm.GetList().Count(y => y.ReceiverMail == mail).ToString();
+            var gonderilensayısı = mm.GetList().Count(y => y.SenderMail == mail).ToString();
+            ViewBag.d1 = gelensayısı;
+            ViewBag.d2 = gonderilensayısı;
+            return View(deger);
+        }
+
+        public ActionResult MessageDetail(int id)
+        {
+            var deger2 = mm.GetList().Where(x => x.MessageId == id).ToList();
+            var mail = (string)Session["CurrentMail"];
+            var deger = mm.GetList().Where(x => x.SenderMail == mail).ToList();
+            var gelensayısı = mm.GetList().Count(y => y.ReceiverMail == mail).ToString();
+            var gonderilensayısı = mm.GetList().Count(y => y.SenderMail == mail).ToString();
+            ViewBag.d1 = gelensayısı;
+            ViewBag.d2 = gonderilensayısı;
+            return View(deger2);
+        }
+    
     }
 }
